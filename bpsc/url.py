7f956e2300e09027d9329f78fb3351ba42bebf81
Vincent Tian <vincentt888@yahoo.com>
2013-10-03 21:14:23 -0700

    removed url.py

--- bpsc/url.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-from mysite.views import hello
-
-urlpatterns = patterns('',
-    ('^/$', home), )
-    ('^survey/$', survey), )
-    ('^search/$', search), )
