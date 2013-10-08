93b553e1cf6981c5d0e358552a56281ffb36ff9a
akakitani <atsunori.kakitani@gmail.com>
2013-10-08 03:11:29 -0700

    Add login/logout views and urls

--- bpsc/common/views.py
+++ bpsc/common/views.py
@@ -1,6 +1,11 @@
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import FormView
+from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
+from django.views import FormView, View
+from django.views.decorators.debug import sensitive_post_parameters
 
 from bpsc.common import SCUserCreationForm
 
@@ -9,3 +14,28 @@ class UserRegistrationView(FormView):
     template_name = 'user_registration.html'
     form_class = SCUserCreationForm
     success_url = reverse_lazy('home')
+
+
+class LoginView(FormView):
+    form_class = AuthenticationForm
+
+    # TODO: Add proper redirection when urls/templates are better defined
+    def form_valid(self, form):
+        auth_login(self.request, form.get_user())
+        if self.request.session.test_cookie_worked():
+            self.request.session.delete_test_cookie()
+        return redirect('home')
+
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
+
+    @method_decorator(sensitive_post_parameters('password'))
+    def dispatch(self, request, *args, **kwargs):
+        request.session.set_test_cookie()
+        return super(LoginView, self).dispatch(request, *args, **kwargs)
+
+
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        auth_logout(request)
+        return redirect('home')
