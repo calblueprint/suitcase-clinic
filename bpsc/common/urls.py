fc700f5a788b421df891f849edfad40dbe1fe7d0
akakitani <atsunori.kakitani@gmail.com>
2013-10-08 03:11:29 -0700

    Add login/logout views and urls

--- bpsc/common/urls.py
+++ bpsc/common/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns
 from django.conf.urls import url
 
-from bpsc.common.views import UserRegistrationView
+from bpsc.common.views import LoginView, LogoutView, UserRegistrationView
 
 urlpatterns = patterns(
     '',
-    url(r'^register/$', UserRegistrationView.as_view(), name='user_registration')
+    url(r'^register/$', UserRegistrationView.as_view(), name='user_registration'),
+    url(r'^login/$', LoginView.as_view(), name='user_login'),
+    url(r'^logout/$', LogoutView.as_view(), name='user_logout'),
 )
 
