0a0acdc010e0c85ad14c97cb6c2a25916c7701ae
akakitani <atsunori.kakitani@gmail.com>
2013-10-22 18:38:09 -0700

    Fixed syntax errors in urls. Commented out reviews b/c buggy, will uncomment later

--- bpsc/search/urls.py
+++ bpsc/search/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns
 from django.conf.urls import url
 
-from bpsc.users.views import (
+from bpsc.search.views import (
     HousingResourceListView, CommunityResourceListView,
     EmploymentResourceListView, LegalResourceListView,
     HousingResourceDetailView, CommunityResourceDetailView,
@@ -12,10 +12,10 @@ from bpsc.users.views import (
 
 urlpatterns = patterns(
     '',
-    url(r'^housing/(?P<pk>\d+)/(?P<slug>[-\w\d]+/$', HousingResourceDetailView.as_view(), name='housing_detail'),
-    url(r'^community/(?P<pk>\d+)/(?P<slug>[-\w\d]+/$', CommunityResourceDetailView.as_view(), name='community_detail'),
-    url(r'^employment/(?P<pk>\d+)/(?P<slug>[-\w\d]+/$', EmploymentResourceDetailView.as_view(), name='employment_detail'),
-    url(r'^legal/(?P<pk>\d+)/(?P<slug>[-\w\d]+/$', LegalResourceDetailView.as_view(), name='legal_detail'),
+    url(r'^housing/(?P<pk>\d+)/(?P<slug>[-\w\d]+)/$', HousingResourceDetailView.as_view(), name='housing_detail'),
+    url(r'^community/(?P<pk>\d+)/(?P<slug>[-\w\d]+)/$', CommunityResourceDetailView.as_view(), name='community_detail'),
+    url(r'^employment/(?P<pk>\d+)/(?P<slug>[-\w\d]+)/$', EmploymentResourceDetailView.as_view(), name='employment_detail'),
+    url(r'^legal/(?P<pk>\d+)/(?P<slug>[-\w\d]+)/$', LegalResourceDetailView.as_view(), name='legal_detail'),
     url(r'^housing/print/$', HousingResourcePrintView.as_view(), name='housing_list'),
     url(r'^community/print/$', CommunityResourcePrintView.as_view(), name='community_list'),
     url(r'^employment/print/$', EmploymentResourcePrintView.as_view(), name='employment_list'),
