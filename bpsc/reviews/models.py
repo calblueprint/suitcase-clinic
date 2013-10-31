414557982da2ff2c3723fa0fcebcf8c5400ceedc
Vincent Tian <vincentt888@yahoo.com>
2013-10-31 00:06:26 -0700

    Basic model formset set up

--- bpsc/reviews/models.py
+++ bpsc/reviews/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Review(models.Model):
-	date = models.DateTimeField()
+	date = models.DateTimeField(auto_now_add=True)
 	service = models.CharField(max_length=50)
-	rating = models.IntegerField()
-	comments = models.TextField()
-	reviewer = models.CharField(max_length=20)
+	rating = models.IntegerField(blank=True)
+	comments = models.TextField(blank=True)
+	reviewer = models.CharField(max_length=20, blank=True)
 
 	def __unicode__(self):
 		return "%(rating)d star review for %(service)s by %(reviewer)s, who says:  %(comments)s" %\
@@ -14,4 +14,10 @@ class Review(models.Model):
 
 
 	# aggregate to get average review
+"""
+class ReviewForm(ModelForm):
+	class Meta:
+		model = Review
+"""
+
 
--- bpsc/reviews/views.py
+++ bpsc/reviews/views.py
@@ -2,20 +2,53 @@ from django.views.generic import ListView
 from bpsc.reviews.models import Review
 from django.views.generic.edit import FormView
 from django.db import models
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
 from bpsc.reviews.forms import ReviewForm
-import datetime
+from bpsc.reviews.models import Review
 
+from django.forms.models import modelformset_factory
+from django.forms.models import BaseModelFormSet
+
+services = ['feet', 'hair']
 
 class ReviewListView(ListView):
 	template_name = 'reviews_list.html'
 	model = Review
 
-class SubmitReviewListView(FormView):
+class SubmitReviewListView(TemplateView):
 	template_name = 'submit_review.html'
-	form_class = ReviewForm
+	# form_class = ReviewForm
 	success_url = '/reviews/reviews'
-
+   
 	def form_valid(self, form):
 		form.submit_review()
-		return super(FormView, self).form_valid(form)
+		return super(FormView, self).form_valid(form)	
+
+	def get_context_data(self, **kwargs):
+		context = super(SubmitReviewListView, self).get_context_data(**kwargs)
+		Review_Formset = modelformset_factory(Review, form=ReviewForm, extra=len(services)) 
+		if self.request.method == "GET":
+			# a = Review_Formset()
+			context['reviewformset'] = Review_Formset()
+			# print(context['reviewformset'])
+			for form, service in zip(context['reviewformset'], services):
+				form.fields['service'].initial = service
+			return context
+		else: # POST requests
+			context['reviewformset'] = Review_Formset(self.request.POST)
+			return context
+
+	# THIS FUNCION IS FOR POST VALIDATION
+	def post(self, request, *args, **kwargs):
+		context = self.get_context_data(**kwargs)
+		reviewformset = context['reviewformset']
+		if reviewformset.is_valid():
+			for entry in reviewformset:
+				if entry.clean_data.get('rating') != null:
+					entry.save()
+			# SENDS USERS TO /REVIEWS/REVIEWS
+			return redirect('reviews/reviews')
+		else:
+			return self.render_to_response(context)
 
