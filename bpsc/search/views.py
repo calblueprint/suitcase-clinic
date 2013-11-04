c2a11a60475b6a93266d3c4dd1dd080c2f69302b
akakitani <atsunori.kakitani@gmail.com>
2013-11-04 02:35:28 -0800

    Add views and forms to print/email resources

--- bpsc/search/views.py
+++ bpsc/search/views.py
@@ -4,6 +4,8 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import DetailView, FormView, ListView
 
+from bpsc.search.forms import ResourcePrintForm
+from bpsc.lib import send_suitcase_email
 from bpsc.search.models import (
     Tag, HousingTag, CommunityTag, EmploymentTag, LegalTag, Resource,
     HousingResource, CommunityResource, EmploymentResource, LegalResource
@@ -106,35 +108,68 @@ class BaseResourcePrintView(ListView):
     tag = Tag
 
     def get_queryset(self):
+        # Get the resources with the IDs in the url querystring
         resource_ids = self.request.GET.getlist('rid')
         queryset = self.model.objects.filter(pk__in=resource_ids)
-        print queryset
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(BaseResourcePrintView, self).get_context_data(**kwargs)
+        if self.request.method == 'POST':
+            context['print_form'] = ResourcePrintForm(self.request.POST)
+        else:
+            context['print_form'] = ResourcePrintForm()
+        return context
+
+
 
     # TODO: Implement POST (printing)
     def post(self, request, *args, **kwargs):
         self.object_list = self.get_queryset()
-        return
+        context = self.get_context_data(object_list=self.object_list)
+        print_form = context['print_form']
+        if print_form.is_valid():
+            email_context_dict = {
+                'client_name': print_form.cleaned_data.get('client_name'),
+                'client_phone': print_form.cleaned_data.get('client_phone'),
+                'client_email': print_form.cleaned_data.get('client_email'),
+                'resource_url': request.build_absolute_uri(),
+                'resource_type': self.resource_type,
+            }
+            send_suitcase_email('print_resource.yml', email_context_dict,
+                    [print_form.cleaned_data.get('user_email')])
+            messages.success(request, 'Resources were printed and emailed successfully.')
+            return redirect(self.success_url)
+        else:
+            return self.render_to_response(context)
 
 class HousingResourcePrintView(BaseResourcePrintView):
     model = HousingResource
     template_name = 'housing_resource_print.html'
+    success_url = 'search:housing_list'
     tag = HousingTag
+    resource_type = 'Housing'
 
 
 class CommunityResourcePrintView(BaseResourcePrintView):
     model = CommunityResource
     template_name = 'community_resource_print.html'
+    success_url = 'search:community_list'
     tag = CommunityTag
+    resource_type = 'Community'
 
 
 class EmploymentResourcePrintView(BaseResourcePrintView):
     model = EmploymentResource
     template_name = 'employment_resource_print.html'
+    success_url = 'search:employment_list'
     tag = EmploymentTag
+    resource_type = 'Employment'
 
 
 class LegalResourcePrintView(BaseResourcePrintView):
     model = LegalResource
     template_name = 'legal_resource_print.html'
+    success_url = 'search:legal_list'
     tag = LegalTag
+    resource_type = 'Legal'
