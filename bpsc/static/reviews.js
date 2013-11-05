2f0d2314aefcc06a6c2352085d0aa7af618c256c
Eugene Kim <maobster@ubuntu.(none)>
2013-11-04 17:44:18 -0800

    start review sorting function

--- bpsc/static/reviews.js
+++ bpsc/static/reviews.js
@@ -22,6 +22,18 @@ function sortOldestFirst(a, b) {
   return date1 > date2 ? 1 : -1;
 }
 
+function sortLowestReviewFirst(a, b) {
+  var date1 = a.children[4].textContent;
+  date1 = date1.split('/');
+  date1 = new Date(date1[2], date1[0], date1[1] - 1);
+  var date2 = b.children[4].textContent;
+  date2 = date2.split('/');
+  date2 = new Date(date2[2], date2[0], date2[1] - 1);
+  return date1 > date2 ? 1 : -1;
+
+  var review1 = a.children
+}
+
 // function sortOldestFirst(a, b) {
 //   var date1 = $(a).find(".review_date").textContent;
 //   date1 = date1.split('/');
