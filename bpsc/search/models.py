9baa5420bde17c5d9a31ecef957d528d2d4802c4
akakitani <atsunori.kakitani@gmail.com>
2013-11-07 22:12:16 -0800

    Add ordering for automatically added resources

--- bpsc/search/models.py
+++ bpsc/search/models.py
@@ -41,6 +41,7 @@ class Resource(models.Model):
     longitude = models.DecimalField(max_digits=8, decimal_places=5, null=True, blank=True)
     phone = models.CharField(max_length=11, blank=True)
     num_used = models.IntegerField(default=0)
+    auto_added = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.name
@@ -55,22 +56,28 @@ class HousingResource(Resource):
     outdated = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ['-posted']
+        ordering = ['auto_added', '-posted']
 
 
 class CommunityResource(Resource):
     tags = models.ManyToManyField(CommunityTag, null=True, blank=True)
+
+    class Meta:
+        ordering = ['auto_added']
 
 
 class EmploymentResource(Resource):
     tags = models.ManyToManyField(EmploymentTag, null=True, blank=True)
     posted = models.DateField()
     outdated = models.BooleanField(default=False)
-    resource_of_the_week = models.BooleanField(default=False)
+    listing_of_the_week = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ['-posted']
+        ordering = ['auto_added', '-posted']
 
 
 class LegalResource(Resource):
     tags = models.ManyToManyField(LegalTag, null=True, blank=True)
+
+    class Meta:
+        ordering = ['auto_added']
