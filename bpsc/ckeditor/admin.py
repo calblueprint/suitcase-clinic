e9a24d7d7f7b2e89f120e2621562daf1e0b795c5
Vincent Tian <vincenttian16@gmail.com>
2013-12-05 01:21:16 -0800

    Changed description of resources to be WYSIWYG

--- bpsc/ckeditor/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from bpsc.ckeditor.models import Post
-
-admin.site.register(Post)
--- bpsc/ckeditor/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns
-from django.conf.urls import url
-
-from bpsc.ckeditor.views import *
-
-urlpatterns = patterns('',
-	# url(r'^post/$', PostView.as_view(), name='post'),
-	# Put anything in this file?
-)
