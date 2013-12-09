f3b4cb56990b53e40874adde2514ed1f2d7aae13
akakitani <atsunori.kakitani@gmail.com>
2013-12-09 09:49:40 -0800

    Implement pop-up client logging

--- bpsc/search/static/js/base_resource_list.js
+++ bpsc/search/static/js/base_resource_list.js
@@ -2,4 +2,23 @@ $(document).ready(function()
     {
         $("#resource-table").tablesorter();
     }
+
+    function getParameter(paramName) {
+      var searchString = window.location.search.substring(1),
+          i, val, params = searchString.split("&");
+      for (i=0;i<params.length;i++) {
+        val = params[i].split("=");
+        if (val[0] == paramName) {
+          return unescape(val[1]);
+        }
+      }
+      return null;
+    }
+
+    $(window).load(function(){
+        if (getParamater('form_error') === 'true') {
+            $('#printModal').modal('show');
+        }
+    });
+
 );
