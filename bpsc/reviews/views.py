3eb3d7f5f17e58a0d5ac03af4f04874a5a162e3b
Eugene Kim <maobster@ubuntu.(none)>
2013-12-10 20:26:48 -0800

    add average reviews for reviews

--- bpsc/reviews/views.py
+++ bpsc/reviews/views.py
@@ -20,6 +20,14 @@ def clean(result):
         else:
             return int(round(result))
 
+def render_stars(rating):
+    result = ""
+    for i in range(0, rating):
+        result += "<span class='glyphicon glyphicon-star'></span>"
+    for i in range(rating, 5):
+        result += "<span class='glyphicon glyphicon-star-empty'></span>"
+    return result
+
 class ReviewListView(ListView):
     template_name = 'reviews_list.html'
     model = Review
@@ -28,13 +36,20 @@ class ReviewListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(ReviewListView, self).get_context_data(**kwargs)
-        context['housing_avg_rating'] = clean(Review.objects.filter(service='Housing').aggregate(Avg('rating'))['rating__avg'])
-        context['employment_avg_rating'] = clean(Review.objects.filter(service='Employment').aggregate(Avg('rating'))['rating__avg'])
-        context['community_avg_rating'] = clean(Review.objects.filter(service='Community Resources').aggregate(Avg('rating'))['rating__avg'])
-        context['legal_avg_rating'] = clean(Review.objects.filter(service='Legal').aggregate(Avg('rating'))['rating__avg'])
-        context['dental_avg_rating'] = clean(Review.objects.filter(service='Dental').aggregate(Avg('rating'))['rating__avg'])
-        context['optometry_avg_rating'] = clean(Review.objects.filter(service='Optometry').aggregate(Avg('rating'))['rating__avg'])
-        context['medical_avg_rating'] = clean(Review.objects.filter(service='Medical').aggregate(Avg('rating'))['rating__avg'])
+        housing_avg_rating = clean(Review.objects.filter(service='Housing').aggregate(Avg('rating'))['rating__avg'])
+        context['housing_stars'] = render_stars(housing_avg_rating)
+        employment_avg_rating = clean(Review.objects.filter(service='Employment').aggregate(Avg('rating'))['rating__avg'])
+        context['employment_stars'] = render_stars(employment_avg_rating)
+        community_avg_rating = clean(Review.objects.filter(service='Community Resources').aggregate(Avg('rating'))['rating__avg'])
+        context['community_stars'] = render_stars(community_avg_rating)
+        legal_avg_rating = clean(Review.objects.filter(service='Legal').aggregate(Avg('rating'))['rating__avg'])
+        context['legal_stars'] = render_stars(legal_avg_rating)
+        dental_avg_rating = clean(Review.objects.filter(service='Dental').aggregate(Avg('rating'))['rating__avg'])
+        context['dental_stars'] = render_stars(dental_avg_rating)
+        optometry_avg_rating = clean(Review.objects.filter(service='Optometry').aggregate(Avg('rating'))['rating__avg'])
+        context['optometry_stars'] = render_stars(optometry_avg_rating)
+        medical_avg_rating = clean(Review.objects.filter(service='Medical').aggregate(Avg('rating'))['rating__avg'])
+        context['medical_stars'] = render_stars(medical_avg_rating)
     # @method_decorator(login_required)
     # def dispatch(self, *args, **kwargs):
     #     return super(ReviewListView, self).dispatch(*args, **kwargs)
--- bpsc/static/js/script.js
+++ bpsc/static/js/script.js
@@ -5,8 +5,49 @@ $(document).ready(function() {
 		transitionSpeed: 500,
 	});
 
+	var clearStars = function() {
+	$('.average').slideUp();;
+	};
+
+	$('#housing-filter').click(function() {
+		clearStars();
+		$('#Housing_Average').slideDown();	
+	});
+
+	$('#employment-filter').click(function() {
+		clearStars();
+		$('#Employment_Average').slideDown();	
+	});
+
+	$('#community-filter').click(function() {
+		clearStars();
+		$('#Community_Average').slideDown();	
+	});
+
+	$('#legal-filter').click(function() {
+		clearStars();
+		$('#Legal_Average').slideDown();	
+	});
+
+	$('#dental-filter').click(function() {
+		clearStars();
+		$('#Dental_Average').slideDown();	
+	});
+
+	$('#optometry-filter').click(function() {
+		clearStars();
+		$('#Optometry_Average').slideDown();	
+	});
+
+	$('#medical-filter').click(function() {
+		clearStars();
+		$('#Medical_Average').slideDown();	
+	});
+
+	$('#all-filter').click(function() {
+		clearStars();
+	});
+
 
 });
-
-var ratingsArray = $('.review-content:has(.review_service:contains("Housing"))').children('rating').text().split('');
 
